9c7e8d95c5d0dd0546a4be2a0b0da42044ec2d07
Tainá Salvador <tainasalvador71@gmail.com>
2025-08-12 19:10:20 -0300

    Exercicio02

---
+++ ExercicioPOO/src/Exercicio02.java
@@ -0,0 +1,24 @@
+public class Exercicio02 {
+    public static void main(String[] args) {
+        Mercado unidadeDeBlumenau = new Mercado();
+        unidadeDeBlumenau.nome = "Super A";
+        unidadeDeBlumenau.macaVendidasPorAno = 800;
+        unidadeDeBlumenau.precoMaca = 12.99;
+        unidadeDeBlumenau.laranjaVendidasPorAno = 1000;
+        unidadeDeBlumenau.precoLaranja = 6.99;
+
+        Mercado unidadeDeJoinville = new Mercado();
+        unidadeDeJoinville.nome = "Super A";
+        unidadeDeJoinville.macaVendidasPorAno = 200;
+        unidadeDeJoinville.precoMaca = 15.99;
+        unidadeDeJoinville.laranjaVendidasPorAno = 850;
+        unidadeDeJoinville.precoLaranja = 6.99;
+
+        Mercado unidadeDeFlorianopolis = new Mercado();
+        unidadeDeFlorianopolis.nome = "Super A";
+        unidadeDeFlorianopolis.macaVendidasPorAno = 500;
+        unidadeDeFlorianopolis.precoMaca = 13.99;
+        unidadeDeFlorianopolis.laranjaVendidasPorAno = 200;
+        unidadeDeFlorianopolis.precoLaranja = 8.99;
+    }
+}
---
+++ ExercicioPOO/src/Mercado.java
@@ -0,0 +1,7 @@
+public class Mercado {
+    String nome;
+    int macaVendidasPorAno;
+    double precoMaca;
+    int laranjaVendidasPorAno;
+    double precoLaranja;
+}
